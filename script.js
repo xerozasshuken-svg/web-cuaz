a969e86426dc77f6fae5e6e2d0dea8a9b048d65a
sam <xerozasshuken@gmail.com>
2026-02-12 21:17:39 -0500

    Add background animation to envelope interaction

--- script.js
+++ script.js
@@ -1,36 +1,42 @@
 const envelope = document.getElementById("envelope");
 const letter = document.getElementById("letter");
 const choices = document.getElementById("choices");
+const background = document.getElementById("background");
 
-// 1️⃣ Aparece el envelope
+//Circulo de fondo
+setTimeout(() => {
+  background.classList.remove("hidden");
+  background.classList.add("show");
+}, 200);
+// Aparece el sobre
 setTimeout(() => {
   envelope.classList.remove("hidden");
   envelope.classList.add("show");
 }, 500);
 
-// 2️⃣ Se abre el envelope
+//Se abre el sobre
 setTimeout(() => {
   envelope.classList.add("open");
 }, 1800);
 
-// 3️⃣ Sale el papel
+//Sale el papel
 setTimeout(() => {
   letter.classList.remove("hidden");
   letter.classList.add("show");
 }, 2600);
 
-// 4️⃣ El envelope desaparece
+//El sobre desaparece
 setTimeout(() => {
   envelope.classList.add("fade-out");
 }, 3400);
 
-// 5️⃣ Aparecen botones
+//Aparecen botones
 setTimeout(() => {
   choices.classList.remove("hidden");
   choices.classList.add("show");
 }, 4200);
 
-// 6️⃣ Al elegir opción
+//Al elegir opción
 document.querySelectorAll(".choices button").forEach(btn => {
   btn.addEventListener("click", () => {
     letter.classList.add("fade-out");
